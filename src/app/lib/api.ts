e9eb83e73384069776aebf20ae33668552e710bf
IsabellePinheiro <isabellepinheiro00@gmail.com>
2024-01-21 20:47:25 -0300

    chore: replace base url to new api

--- src/app/lib/api.ts
+++ src/app/lib/api.ts
@@ -1,16 +1,18 @@
 import { Trip } from "@/app/page";
 
+export const BASE_URL = "https://chooose-api.vercel.app";
+
 export const getTrips = async ({
   page = 1,
 }: {
   page?: number;
 }): Promise<Trip[]> => {
-  const response = await fetch(`http://localhost:8000/trips?_page=${page}`);
+  const response = await fetch(`${BASE_URL}/trips?_page=${page}`);
   return await response.json();
 };
 
 export const getTripDetails = async (id: string): Promise<Trip> => {
-  const response = await fetch(`http://localhost:8000/trips/${id}`);
+  const response = await fetch(`${BASE_URL}/trips/${id}`);
   const data = await response.json();
   return data;
 };
